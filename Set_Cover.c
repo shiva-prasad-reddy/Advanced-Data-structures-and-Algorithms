0ed99dc9a08ba850d5b8f17c03d348f23e5c03cb
Shiva Reddy <puchalashivaprasadreddy@gmail.com>
2019-10-09 22:34:37 +0530

    --

--- Set_Cover.c
+++ Set_Cover.c
@@ -6,7 +6,6 @@
 #define FALSE 0
 #define MAX_SET_MEMBERS 20
 
-
 //IN ANY SET AT INDEX [0] THE NO. OF ELEMENTS PRESENT IN THAT SET ARE STORED
 
 int **initializeSets(int no_of_sets)
@@ -24,59 +23,55 @@ int **initializeSets(int no_of_sets)
 		set[0] = input;
 		container[i] = set;
 
-		printf("Enter the [ %d ] elements of set [ %d ]\n", input, i);
+		printf("Enter the %d elements of set [ %d ]\n", input, i);
 		for(int j = 1; j <= set[0]; j++)
 		{
 			scanf("%d", &input);
 			set[input] = TRUE;
 		}
 		printf("\n");
-
 	}
-
 
 	for(int i = 0; i < no_of_sets; i++)
 	{
 		set = container[i];
-		printf("\n Elements in Set [ %d ]", i);
+		printf("\n Elements in Set [ %d ] >> ", i);
 		for(int j = 1; j < MAX_SET_MEMBERS; j++)
 			if(set[j] == TRUE) printf("%d, ", j);
 
 	}
-
+	printf("\n");
 	return container;
 }
 
-
-
 int find_set_containing_max_elements(int **container, int no_of_sets)
 {
-	int max_index = 1;
-	for(int i = 2; i < no_of_sets; i++)
-		if(container[i][0] > container[max_index][0])
-			max_index = i;
+	int max_set_index = 1;
+	for(int i = 1; i < no_of_sets; i++)
+		if(container[i][0] > container[max_set_index][0])
+			max_set_index = i;
 
-	return max_index;
+	return max_set_index;
 }
 
-void remove_elements(int **container, int no_of_sets, int max_set_index, int *answer_set)
+//	complexity can be reduced by taking the answer_set and 
+//	not visiting the answer_set which is already considered
+//	if(answer_set[j] == TRUE) continue; //not much complexity reduced --- even ERROR
+//															because making it TRUE before removin elements
+
+void remove_elements(int **container, int no_of_sets, int max_set_index)
 {
-
-	//complexity can be reduced by taking the answer_set and not visiting the answer_set 
-	//which is already considered
-
 	int *max_set = container[max_set_index];
 	for(int i = 1; i < MAX_SET_MEMBERS; i++)
 	{
 		if(max_set[i] == TRUE)
 		{
-
 			for(int j = 0; j < no_of_sets; j++)
 			{
 				if(container[j][i] == TRUE)
 				{
 					container[j][i] = FALSE;
-					container[j][0] -= 1;
+					container[j][0] = container[j][0] - 1;
 				}
 			}
 
@@ -96,14 +91,13 @@ void set_covering_approximation_algorithm(int **container, int no_of_sets)
 		remove_elements(container, no_of_sets, max_set_index);
 	}
 
-	printf("Approx. Answer Set for Set Covering Problem\n");
+	printf("\n\nApprox. Answer Set for Set Covering Problem\n");
 	for(int i = 1; i < no_of_sets; i++)
 		if(answer_set[i] == TRUE) printf("SET [ %d ]\n", i);
 
 	free(answer_set);
 
 }
-
 
 int main()
 {
@@ -115,6 +109,9 @@ int main()
 
 	set_covering_approximation_algorithm(container, no_of_sets);
 
+	for(int i = 0; i < no_of_sets; i++) free(container[i]);
+	free(container);
+	printf("\n");
 	return 0;
 }
 
